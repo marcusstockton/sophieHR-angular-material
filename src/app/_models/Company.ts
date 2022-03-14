fab701230cf48edb50a21f152f10f925e954001c
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2022-03-14 15:27:21 +0000

    WIP

--- src/app/_models/Company.ts
+++ src/app/_models/Company.ts
@@ -1,15 +1,24 @@
-export class Company{
+export class Company {
     id: string;
     name: string;
     logo?: string;
-    createdDate:Date;
-    updatedDate:Date;
+    createdDate: Date;
+    updatedDate: Date;
+    employeeCount: number;
     address: {
+        id: string;
+        addressType: AddressType;
         line1: string;
         line2: string;
-        line3:string;
-        line4:string;
-        postcode:string;
-        county:string;
+        line3: string;
+        line4: string;
+        postcode: string;
+        county: string;
     }
+}
+
+
+export enum AddressType {
+    Company,
+    Employee
 }
