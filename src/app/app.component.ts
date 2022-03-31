30096ac670275348242eda82d6bd5f231709eee1
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2022-03-31 19:56:33 +0100

    Styling sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenStorageService } from './_services/token-storage.service';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { UserService } from './_services/user.service';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +19,7 @@ export class AppComponent {
   companyNames: { id: string, name: string }[] = [];
   companyId?: string;
 
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
   constructor(public tokenStorageService: TokenStorageService, private router: Router, private http: HttpClient, private userService: UserService) { }
 
   ngOnInit(): void {
@@ -27,9 +28,9 @@ export class AppComponent {
       const user = this.tokenStorageService.getUser();
       this.role = user.role;
       this.username = user.username;
-      if (this.role === "Admin") {
-        this.getCompanies();
-      }
+      // if (this.role === "Admin") {
+      //   this.getCompanies();
+      // }
     }
   }
   logout(): void {
@@ -41,25 +42,25 @@ export class AppComponent {
     this.router.navigate(['/profile'])
   }
 
-  getCompanies() {
-    this.http.get(environment.base_url + "/Companies")
-      .pipe(
-        map((res: any) => {
-          res.forEach((element: any) => {
-            this.companyNames.push({id: element.id, name: element.name});
-          });
-        })).subscribe();
-  }
+  // getCompanies() {
+  //   this.http.get(environment.base_url + "/Companies")
+  //     .pipe(
+  //       map((res: any) => {
+  //         res.forEach((element: any) => {
+  //           this.companyNames.push({id: element.id, name: element.name});
+  //         });
+  //       })).subscribe();
+  // }
 
-  onCompanyChange(event:any){
-    console.log("You clicked on company id: " + event.value);
-    this.userService.updateCompanyId(event.value);
-    // Emit this to the admin page?
-  }
+  // onCompanyChange(event:any){
+  //   console.log("You clicked on company id: " + event.value);
+  //   this.userService.updateCompanyId(event.value);
+  //   // Emit this to the admin page?
+  // }
 
-  clearCompany($event: any) {
-    this.companyId = undefined;
-    $event.stopPropagation();
-    this.userService.updateCompanyId("");
-  }
+  // clearCompany($event: any) {
+  //   this.companyId = undefined;
+  //   $event.stopPropagation();
+  //   this.userService.updateCompanyId("");
+  // }
 }
