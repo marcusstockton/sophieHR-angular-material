4b068cd94fbc352e125b55da44c0283ade34a8e7
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2022-06-13 10:50:56 +0100

    Adding some unit tests to auth guard

--- src/app/_helpers/auth.guard.spec.ts
+++ src/app/_helpers/auth.guard.spec.ts
@@ -1,14 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { inject, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
-
+  const mockTokenStorageService = jasmine.createSpyObj('TokenStorageService', ['isLoggedIn'])
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[RouterTestingModule],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: TokenStorageService, useValue: mockTokenStorageService },
+      ]
     });
     guard = TestBed.inject(AuthGuard);
   });
@@ -16,4 +22,19 @@ describe('AuthGuard', () => {
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+  it('should navigate', inject([Router], (router: Router) => {
+    spyOn(router, 'navigate').and.stub();
+    mockTokenStorageService.isLoggedIn = false;
+    guard.canActivate();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    expect(guard.canActivate()).toBeFalse();
+  }));
+
+  it('should return true', inject([Router], (router: Router) => {
+    spyOn(router, 'navigate').and.stub();
+    mockTokenStorageService.isLoggedIn = true;
+    guard.canActivate();
+    expect(guard.canActivate()).toBeTrue();
+  }));
+
 });
