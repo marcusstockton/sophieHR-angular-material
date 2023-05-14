7d9d8d1f2b87e6a2ad9cd37c7fe13bbad5220a1a
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2023-05-14 21:24:40 +0100

    update angular from 15 to 16

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { TokenStorageService } from '../_services/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private _tokenService: TokenStorageService, private _router: Router) { }
   canActivate(): boolean {
     if (this._tokenService.isLoggedIn) {
