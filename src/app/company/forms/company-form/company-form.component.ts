0048dc786403787a59608fa11b3c431e70da951c
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2023-05-27 18:15:17 +0100

    Updating client to latest version of the api. Added form to comp form and started work on the page

--- src/app/company/forms/company-form/company-form.component.ts
+++ src/app/company/forms/company-form/company-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { CompaniesClient, CompanyDetailDto } from 'src/app/client';
 
 @Component({
   selector: 'app-company-form',
@@ -10,7 +12,26 @@ export class CompanyFormComponent implements OnInit {
 
   public companyId:string|null;
   public editing:boolean;
-  constructor(private route: ActivatedRoute) { }
+  public companyDetails: CompanyDetailDto;
+  public readonly companyForm: FormGroup;
+
+
+  constructor(private route: ActivatedRoute, private _companyService: CompaniesClient, private readonly formBuilder: FormBuilder) {
+
+    this.companyForm = this.formBuilder.group({
+      name: [],
+      address: this.formBuilder.group({
+        line1: [],
+        line2: [],
+        line3: [],
+        line4: [],
+        postcode: [],
+        county: [],
+      }),
+      myRequiredField: ['', Validators.required],
+    });
+
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -18,7 +39,20 @@ export class CompanyFormComponent implements OnInit {
         this.editing = true;
       }
       this.companyId = params['companyid'];
+      this._companyService.getCompany(this.companyId!).subscribe(
+        res => {
+          this.companyDetails = res
+          this.companyForm.patchValue(res);
+        },
+        err => console.log('HTTP Error', err),
+        () => console.log('HTTP request completed.')
+      )
+
     });
+  }
+
+  submitCompany(){
+    console.log(this.companyForm)
   }
 
 }
